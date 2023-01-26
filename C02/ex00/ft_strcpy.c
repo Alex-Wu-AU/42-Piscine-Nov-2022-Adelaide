5bc3e851314c8585c428ec3eb0136c149b3d5a60
Alex-Wu-AU <alex.nanwu@gmail.com>
2023-01-26 13:54:22 +1100

    C02 final submission

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 22:28:43 by nwu               #+#    #+#             */
+/*   Updated: 2022/12/06 16:35:58 by nwu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int main()
+{
+	char src[]	= "Hello";
+	char dest[] ="its a long string";
+	char* src_ptr = &src[0];
+	char* dest_ptr = &dest[0];
+
+	printf("%s\n", "BEFORE");
+	printf("source: %s\n", src);
+	printf("destination: %s\n", dest);
+	printf("\n%s\n", "AFTER");
+	ft_strcpy(dest_ptr, src_ptr);
+	printf("source: %s\n", src);
+    printf("destination: %s\n", dest);
+	return (0);
+}
